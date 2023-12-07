c0209e5c86a52c09996000e0bafa249b1708a46a
Oleksa2106 <obitelteney@gmail.com>
2023-12-07 22:05:41 +0200

    add search by car id

--- index.js
+++ index.js
@@ -14,18 +14,37 @@ app.use("/auth", authRouter);
 const start = async () => {
     try {
         await mongoose.connect('mongodb+srv://vlad88998:vlad88998@cluster1.whkatcr.mongodb.net/?retryWrites=true&w=majority')
-        app.listen(PORT, () => console.log(`server started on port ${PORT}`)) 
-       // await mongoose.connect('mongodb://127.0.0.1:27017/auth_roles')
+        app.listen(PORT, () => console.log(`server started on port ${PORT}`))
+        // await mongoose.connect('mongodb://127.0.0.1:27017/auth_roles')
 
         app.get("/get", (req, res) => {
             CarModel.find()
-            .then(result => res.json(result))
-            .catch(err => res.json(err))
+                .then(result => res.json(result))
+                .catch(err => res.json(err))
         })
-  
+
+        // Маршрут для отримання інформації про автомобіль за id
+        app.get('/api/cars/:id', async (req, res) => {
+            try {
+                const carId = req.params.id
+
+                // Знайдіть автомобіль за id в базі даних
+                const car = await CarModel.findById(carId);
+
+                if (!car) {
+                    return res.status(404).json({ message: 'Автомобіль не знайдено' });
+                }
+
+                res.json(car);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ message: 'Помилка сервера' });
+            }
+        });
+
     } catch (e) {
         console.log(e)
-    } 
+    }
 }
 
 start()
